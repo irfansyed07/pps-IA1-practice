f661998b86bafb82d4196ab60a1207a9a75d2a6a
irfansyed07 <93091192+irfansyed07@users.noreply.github.com>
2022-02-02 14:40:41 +0530

    Update p1original.c

--- p1original.c
+++ p1original.c
@@ -1,2 +1,27 @@
-
+#include <stdio.h>
+int input()
+{
+  int a;
+  printf("enter a number\n");
+  scanf("%d",&a);
+  return a;
+}
+int add(int a, int b)
+{
+  int c;
+  c=a+b;
+  return c;
+}
+void output(int c)
+{
+printf("addition of two numbers is %d", c);
+}
+int main()
+{
+int x,y,z;
+x= input ();
+y=input();
+z=add(x,y);
+output (z);
+return 0 ;
 }
